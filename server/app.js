ba644a98739c181415937ed26a56bdf7edd3b421
yuriy_yupiter <yu.piter.gm@gmail.com>
2022-02-21 03:59:51 +0300

    feat: setting up a working environment

--- server/app.js
+++ server/app.js
@@ -6,6 +6,12 @@ const chalk = require("chalk");
 const app = express();
 const PORT = config.get("port") ?? 8080;
 
+if (process.env.NODE_ENV === "production") {
+    console.log(chalk.bgCyanBright("Production"));
+} else {
+    console.log(chalk.bgCyanBright("Development"));
+}
+
 app.listen(PORT, (req, res) => {
     console.log(chalk.bgGreen(`Server has been started on port ${PORT}...`));
 });
