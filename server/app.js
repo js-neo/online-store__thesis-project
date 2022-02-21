58ccc1d3a981cf3781902ff9d33f385261df68f7
yuriy_yupiter <yu.piter.gm@gmail.com>
2022-02-21 04:06:10 +0300

    feat: add middleware for application

--- server/app.js
+++ server/app.js
@@ -4,6 +4,10 @@ const config = require("config");
 const chalk = require("chalk");
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 const PORT = config.get("port") ?? 8080;
 
 if (process.env.NODE_ENV === "production") {
