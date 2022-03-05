b5c5a552d5489170a58bc919fa9899743aaaf701
yuriy_yupiter <yu.piter.gm@gmail.com>
2022-03-05 13:39:59 +0300

    feat: add images products

--- client/src/data.js
+++ client/src/data.js
@@ -8,14 +8,14 @@ export const sliderItems = [
   },
   {
     id: 2,
-    img: "https://i.ibb.co/DG69bQ4/2.png",
+    img: "https://github.com/Yupiter78/online-store__thesis-project/blob/main/client/src/assets/girl_in_red_pants.png?raw=true",
     title: "AUTUMN COLLECTION",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "fcf1ed",
   },
   {
     id: 3,
-    img: "https://i.ibb.co/cXFnLLV/3.png",
+    img: "https://github.com/Yupiter78/online-store__thesis-project/blob/main/client/src/assets/girl_in_shorts.png?raw=true",
     title: "LOUNGEWEAR LOVE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "fbf0f4",
